0e90f9760643dc4fd8cd9aa7aa5405fe8e172a74
unknown <lauraocampo353@gmail.com>
2024-03-22 11:05:45 -0400

    comment

--- script.js
+++ script.js
@@ -1,52 +1,36 @@
 const tracklist = document.getElementById("track-list")
 
+
 axios.get("https://leonardoapi.onrender.com/songs")
     .then((res) => {
-        console.log(res.data.songs)
+
 
         res.data.songs.map((song) => {
 
-            console.log(song.path.front)
+
 
             const div = document.createElement("div")
-
+            div.classList.add("cover-info")
 
             div.innerHTML = `
-                <img src="${song.path.front}" alt="" class="h-full"> </img>
-            $(song.title)
+            <img class="covers" src="${song.path.front}" alt="">
+            
+            <div class="info-songs"> 
+                <h2>${song.title}</h2>
+                <p>${song.author}</p>
+            </div>
+            <p>${song.duration}</p>
+            
+            </div>
+        
             `
-
-
             tracklist.appendChild(div)
 
         })
 
 
+
     })
-
-
-
-const handlerclick = () => {
-}
-
-
-// handlerclick ()
-
-const button = document.getElementById("handler-button")
-
-
-if (button !== null) {
-
-
-    button.addEventListener("click", () => {
-        const cadena = "esto es una cadena de texto"
-        console.log(cadena)
-
-    }
-
-    )
-}
-
 
 
 
